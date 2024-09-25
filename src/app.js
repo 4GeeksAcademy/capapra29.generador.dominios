6c3cc63a248ce863909c4424906b0bea5732d0c7
capapra29 <capapra29@gmail.com>
2024-09-25 19:51:02 +0000

    generador de dominios

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".net", ".us", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let h = 0; h < adj.length; h++) {
+      for (let j = 0; j < noun.length; j++) {
+        for (let k = 0; k < ext.length; k++) {
+          console.log(pronoun[i] + adj[h] + noun[j] + ext[k]);
+        }
+      }
+    }
+  }
 };
